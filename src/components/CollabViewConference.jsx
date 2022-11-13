cd7803667511884d9e8a63eabc4ea6a19d527d70
Atheesh Thirumalairajan <github-commits@atheesh.org>
2022-11-13 22:27:45 +0530

    Conference Pushes

--- src/components/CollabViewConference.jsx
+++ src/components/CollabViewConference.jsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import VideocamOffIcon from '@mui/icons-material/VideocamOff';
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import SettingsIcon from '@mui/icons-material/Settings';
 import * as virtClassroom from "mediasoup-client";
 
 const CollabViewConference = (props) => {
     const [AVStreams, SetAVStreams] = React.useState([]);
+    const [videoStreaming, setVideoStreaming] = React.useState(false);
+    const [audioStreaming, setAudioStreaming] = React.useState(false);
     const [compatibilityErr, setCompatibilityErr] = React.useState(false);
 
     //Initialize SFU Server Connection...
@@ -48,7 +57,8 @@ const CollabViewConference = (props) => {
                     style={{
                         width: '100%',
                         height: '100%',
-                        objectFit: 'cover'
+                        objectFit: 'cover',
+                        background: '#000000'
                     }}
                 >
                 </video>
@@ -63,6 +73,13 @@ const CollabViewConference = (props) => {
 
     return (
         <Box sx={{ ...props.sx, height: '100%', flexDirection: 'column' }}>
+            <Box sx={{ padding: '8px' }}>
+                <ButtonGroup variant="outlined" fullWidth>
+                    <Button onClick={() => { setVideoStreaming(!videoStreaming) }}>{(videoStreaming === true ? <VideocamOffIcon /> : <VideocamIcon />)}</Button>
+                    <Button onClick={() => { setAudioStreaming(!audioStreaming) }}>{(audioStreaming === true ? <MicOffIcon /> : <MicIcon />)}</Button>
+                    <Button><SettingsIcon /></Button>
+                </ButtonGroup>
+            </Box>
             <Box sx={{ padding: '8px' }}>
                 {
                     AVStreams.map((AVStream) => {
