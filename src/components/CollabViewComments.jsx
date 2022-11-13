cd7935e98166a30c342cc44faa768a2668e1a423
Atheesh Thirumalairajan <github-commits@atheesh.org>
2022-11-13 15:17:05 +0530

    Progression in VC

--- src/components/CollabViewComments.jsx
+++ src/components/CollabViewComments.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import SmsFailedIcon from '@mui/icons-material/SmsFailed';
 import SendIcon from "@mui/icons-material/Send";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -46,6 +47,10 @@ const CollabViewComments = (props) => {
     return (
         <Box sx={{ ...props.sx, height: '100%', flexDirection: 'column' }}>
             <Box sx={{ padding: '8px', flexGrow: 1, maxHeight: '100%', overflowY: 'auto' }}>
+                <Box sx={{ display: (comments.length == 0 ? 'flex' : 'none'), flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', color: 'text.disabled' }}>
+                    <SmsFailedIcon fontSize='large'></SmsFailedIcon>
+                    <Typography variant="h6">No Messages</Typography>
+                </Box>
                 {
                     comments.map((comment) => {
                         return (
