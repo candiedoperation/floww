9a2cd1181963ce2c1aae6dae8087934d8e27dce6
Atheesh Thirumalairajan <github-commits@atheesh.org>
2022-11-15 21:30:47 +0530

    Added Tab Elements

--- src/integrals/CollabViewEntry.jsx
+++ src/integrals/CollabViewEntry.jsx
@@ -6,6 +6,8 @@ import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab';
 import AddIcon from '@mui/icons-material/Add';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
 import * as React from 'react';
 
 const CollabViewEntry = () => {
@@ -13,13 +15,20 @@ const CollabViewEntry = () => {
 
     const CreateRoomPanel = (internal_props) => {
         return (
-            <Box sx={{ ...internal_props.sx }}>A</Box>
+            <Box sx={{ ...internal_props.sx, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', height: '100%', minHeight: '200px'}}>
+                <TextField sx={{ width: '50%' }} label="Name" variant="outlined" />
+                <Button startIcon={<AddIcon />} sx={{ marginTop: '15px' }} variant="contained">Create and Join Room</Button>
+            </Box>
         )
     }
 
     const JoinRoomPanel = (internal_props) => {
         return (
-            <Box sx={{ ...internal_props.sx }}>B</Box>
+            <Box sx={{ ...internal_props.sx, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', height: '100%', minHeight: '200px'}}>
+                <TextField sx={{ width: '50%', marginBottom: '10px' }} label="Room ID" variant="outlined" />
+                <TextField sx={{ width: '50%' }} label="Name" variant="outlined" />
+                <Button startIcon={<MeetingRoomIcon />} sx={{ marginTop: '15px' }} variant="contained">Join Room</Button>
+            </Box>
         )
     }
 
@@ -33,20 +42,21 @@ const CollabViewEntry = () => {
                         <Typography variant='p'>Virtual Classroom</Typography>
                     </Box>
                 </Grid>
+                <Divider orientation='vertical' sx={{ marginRight: '-1px' }} />
                 <Grid item xs={9}>
-                    <Box sx={{ boxShadow: 8, width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <Paper sx={{ width: '70%', height: '60%' }} variant="outlined">
+                    <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                        <Paper sx={{ width: '70%', height: '60%', display: 'flex', flexDirection: 'column' }}>
                             <Tabs
                                 variant='fullWidth'
                                 value={currentTab}
                                 onChange={(e, newValue) => { setCurrentTab(newValue) }}
                                 aria-label="Vertical tabs example"
-                                sx={{ borderColor: 'divider' }}
+                                sx={{ borderColor: 'divider', minHeight: '72px' }}
                             >
                                 <Tab icon={<AddIcon />} label="Create new Room" aria-label="Create new Room" />
                                 <Tab icon={<MeetingRoomIcon />} label="Join existing Room" aria-label="Join existing Room" />
                             </Tabs>
-                            <Box>
+                            <Box sx={{ flexGrow: 1, overflow: 'auto'  }}>
                                 <CreateRoomPanel sx={{ display: (currentTab == 0 ? 'flex' : 'none') }}></CreateRoomPanel>
                                 <JoinRoomPanel sx={{ display: (currentTab == 1 ? 'flex' : 'none') }}></JoinRoomPanel>
                             </Box>
