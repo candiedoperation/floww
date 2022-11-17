714fd9b708e2e7a6a82f3436e8730d7eab4a15f6
Atheesh Thirumalairajan <github-commits@atheesh.org>
2022-11-17 22:19:00 +0530

    designs and more

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
+import * as React from 'react';
+import axios from "axios";
 import CollabViewEntry from "./integrals/CollabViewEntry";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Cookies from 'universal-cookie';
 import HomeView from "./integrals/HomeView";
 import HomeLogin from "./integrals/HomeLogin";
+import { serverURL } from "./middleware/FlowwServerParamConn";
+import { Box, LinearProgress, Typography } from '@mui/material';
 
 const CheckAuth = (props) => {
-    const cookies = new Cookies();
-    if (cookies.get("authKey")) {
-        return (
-          props.children
-        )
-    } else {
+    const [authenticated, setAuthenticated] = React.useState(0);
+
+    React.useEffect(() => {
+      axios
+        .post(`${serverURL}/api/verifyauth`)
+        .then((res) => {
+          const flowwResponse = res.response.data;
+          (flowwResponse.status === true) ? setAuthenticated(2) : setAuthenticated(1);
+        })
+        .catch((res) => {
+          setTimeout(() => {
+            setAuthenticated(1);
+          }, 1200);
+        })
+    }, [])
+
+    const LoadingPageElement = () => {
       return (
-        <Navigate to="/login"></Navigate>
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+          <Typography variant="h6">🧐 Confirming it's you!</Typography>
+          <LinearProgress sx={{ width: '80%' }} />
+        </Box>
       )
+    }
+
+    switch (authenticated) {
+      case 0: return (<LoadingPageElement />)
+      case 1: return (<Navigate to="/login" />)
+      case 2: return (props.children)
     }
 }
 
